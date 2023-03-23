334fa7862448abc2b253596b1d7f7c8cc9c1ca1c
pcokay <128701731+pcokay@users.noreply.github.com>
2023-03-23 17:33:45 +0200

    Update webpack.functions.js

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src_functions/handler.js', // Make sure this is the correct path to your handler.js file
+  entry: './src_functions/handler.js', // This should be the correct path to your handler.js file
   output: {
     filename: 'handler.js',
     path: path.resolve(__dirname, 'functions_build'),

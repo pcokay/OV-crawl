28f3e0aa036732b28045843b1855ad68b159324d
pcokay <128701731+pcokay@users.noreply.github.com>
2023-03-23 17:28:34 +0200

    Update webpack.functions.js

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src_functions/app.py',
+  entry: './src_functions/handler.js', // Make sure this is the correct path to your handler.js file
   output: {
-    path: path.join(__dirname, 'functions_build'),
-    filename: 'app.js',
+    filename: 'handler.js',
+    path: path.resolve(__dirname, 'functions_build'),
+    libraryTarget: 'commonjs',
+  },
+  target: 'node',
+  mode: 'production',
+  externals: {
+    'aws-sdk': 'aws-sdk',
   },
 };
